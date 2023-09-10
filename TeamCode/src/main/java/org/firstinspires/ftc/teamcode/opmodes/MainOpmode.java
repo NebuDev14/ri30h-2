b0d04d0f12328b8b3e407d1db09c767fc0deb4a3
warren yun <wyun2006@gmail.com>
2023-09-10 14:52:09 -0400

    base project structure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainOpmode.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class MainOpmode extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
